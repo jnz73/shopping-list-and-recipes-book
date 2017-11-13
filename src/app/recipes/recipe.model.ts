7ee918be34c4e81e45fa54253391f1365ba1ce34
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2017-11-13 16:53:33 +0100

    add show all the recipes

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -2,4 +2,10 @@ export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
+
+    constructor(name, description, imagepath) {
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagepath;
+    }
 }
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,7 +8,9 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class RecipesListComponent implements OnInit {
-  recipies = [];
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Test', 'This is a test description', 'http://www.seriouseats.com/recipes/assets_c/2015/01/20150119-pressure-cooker-chicken-stew-food-lab-11-thumb-1500xauto-418088.jpg')];
   constructor() { }
 
   ngOnInit() {
