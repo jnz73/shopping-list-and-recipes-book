847dd1bb0f8999b1acb0ff98a282c0474b32d8eb
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2017-11-13 15:54:08 +0100

    add modules stubs

---
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-detail',
+  templateUrl: './recipes-detail.component.html',
+  styleUrls: ['./recipes-detail.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class RecipesDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class RecipesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
