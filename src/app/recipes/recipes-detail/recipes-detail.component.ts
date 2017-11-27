2fee804ed18f7e74c3aae5f4008711609e9eb806
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2017-11-27 15:51:10 +0100

    add routing for edit/new recipe

--- src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipes-detail',
@@ -13,7 +13,7 @@ export class RecipesDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) {
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit() {
@@ -28,5 +28,9 @@ export class RecipesDetailComponent implements OnInit {
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
 
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 }
