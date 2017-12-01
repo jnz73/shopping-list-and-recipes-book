aff11c02a318eed8bde763b0c343d49cc929c814
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2017-12-01 11:41:46 +0100

    add Home Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {HomeComponent} from './home/home.component';
 
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: '', component: HomeComponent},
   {path: 'shopping-list', component: ShoppingListComponent}, // not necessary to extract in another routing module
 
 ];
